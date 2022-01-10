050888b04cdaf9abb3b323c2ebf08b2310e03077
pipeline23 <62163126+pipeline23@users.noreply.github.com>
2022-01-10 13:46:02 -0300

    Add files via upload
    10/01/22

---
+++ Lugares/src/app/app-routing.module.ts
@@ -0,0 +1,57 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    children:[
+    {
+      path:"",
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    },
+    {
+      path: ":placeId",
+      loadChildren: () => import('./pages/lugar/lugar.module').then(m => m.LugarPageModule)
+    }
+
+  ]
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'agregar',
+    loadChildren: () => import('./pages/agregar/agregar.module').then( m => m.AgregarPageModule)
+  },
+  {
+    path: 'registro',
+    loadChildren: () => import('./pages/registro/registro.module').then( m => m.RegistroPageModule)
+  },
+  {
+    path: 'lugar',
+    loadChildren: () => import('./pages/lugar/lugar.module').then( m => m.LugarPageModule)
+  },
+  {
+    path: 'perfil',
+    loadChildren: () => import('./pages/perfil/perfil.module').then( m => m.PerfilPageModule)
+  },
+  {
+    path: 'editar',
+    loadChildren: () => import('./pages/editar/editar.module').then( m => m.EditarPageModule)
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
